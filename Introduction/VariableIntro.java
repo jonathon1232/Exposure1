b996d06454ac09b8283aa6e48bac6eb137b7e41a
mrkiedes <akiedes@vtsd.com>
2025-05-29 11:52:55 -0400

    Initial Upload

---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,27 @@
+package Introduction;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        /*
+         * To Create a variable:
+         * datatype name = value;
+         */
+        double salary = 10000.56;
+        int bonus = 250;
+
+        /*
+         * Casting a variable means changing its datatype
+         * to change a primitive data type, put the new type in ()
+         * Example below.
+         */
+
+        bonus = (int) salary;
+
+        System.out.println(salary);
+        System.out.println(bonus);
+    }
+
+
+}
---
+++ Lab03/Lab03.java
@@ -0,0 +1,11 @@
+package Lab03;
+
+public class Lab03 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
